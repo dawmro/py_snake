e8c463ec4e7d43ccc537cf5cff80f0e1345c017c
dawid mrotek <DawidMrotek@protonmail.ch>
2023-01-05 02:36:52 +0100

    add snake class

---
+++ game_objects.py
@@ -0,0 +1,19 @@
+# game objects file
+
+import pygame as pg
+
+
+# class of game object called snake
+class Snake:
+    def __init__(self, game):
+        pass
+        
+    # method to update snake state  
+    def update_state(self):
+        pass
+        
+    # method to draw snake object
+    def draw_object(self):
+        pass
+        
+    
