b972a9fa1dad824a378d2b5356b140fa53848c8a
dawid mrotek <DawidMrotek@protonmail.ch>
2023-01-05 02:19:54 +0100

    add main game class

---
+++ main.py
@@ -0,0 +1,30 @@
+# py_snake main file
+
+import pygame as pg
+
+
+# main game class
+class Game: 
+    def __init__(self):
+        pass
+    
+    # create new game
+    def new_game(self):
+        pass
+    
+    # update game state
+    def update_state(self):
+        pass
+    
+    # draw game object
+    def draw_object(self):
+        pass
+        
+    # check for events
+    def event_check(self):
+        pass
+        
+    # start game run
+    def run(self):
+        pass
+    
